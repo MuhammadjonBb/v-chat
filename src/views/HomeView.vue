153155fc16c20d3d85d9eb66d98231beba1209f2
Muhammad Boborakhimov <muhammadzonboborahimov7@gmail.com>
2023-08-15 21:41:37 +0500

    INIT: base model

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import SearchPanel from '@/components/SearchPanel.vue';
+import UsersList from '@/components/UsersList.vue';
+
+</script>
+
+<template>
+  <main class="main">
+    <div class="users">
+      <SearchPanel />
+      <UsersList />
+    </div>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+.users {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
